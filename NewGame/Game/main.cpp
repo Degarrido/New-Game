c5d04774125e6c8f75a5366ea7d78b5a6469f842
Degarrido <de.garrido95@gmail.com>
2018-10-16 10:43:46 -0300

    PlayerMovement Added

--- NewGame/Game/main.cpp
+++ NewGame/Game/main.cpp
@@ -3,5 +3,28 @@
 using namespace std;
 
 int main() {
+	int screenWidth = 800;
+	int screenHeight = 450;
 
+	InitWindow(screenWidth, screenHeight, "SideScroller");
+
+	int playerPosition = screenHeight / 2 - 200 ;
+	int scrollSpeed = 10;
+
+	SetTargetFPS(60);
+
+	while (!WindowShouldClose()) {
+
+		if (IsKeyDown(KEY_UP)) playerPosition -= scrollSpeed;
+		if (IsKeyDown(KEY_DOWN)) playerPosition += scrollSpeed;
+
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+		DrawRectangle(screenWidth / 2 - 200, playerPosition, 80, 80, BLUE);
+		EndDrawing();
+	}
+
+	CloseWindow();
+
+	return 0;
 }
