ee9f88227b5059b47a4ef998997fd5b6a62b96d9
Degarrido <de.garrido95@gmail.com>
2018-10-23 10:54:10 -0300

    Charged Player Texture

--- ChangeLog.md
+++ ChangeLog.md
@@ -1,7 +1,10 @@
 ## 0.0.1 - 2018-10-16
 ### Added
 - Player Movement.
+## 0.0.2 - 2018-10-23
+- Load Image Function.
 
 [Unreleased]
 -Create Enemies.
 -Enemies Movement.
+-Player Texture.
--- NewGame/Game/main.cpp
+++ NewGame/Game/main.cpp
@@ -7,6 +7,11 @@ int main() {
 	int screenHeight = 450;
 
 	InitWindow(screenWidth, screenHeight, "SideScroller");
+	Texture2D spaceShip = LoadTexture("res/SpaceShip.png");
+
+
+	Rectangle frameRec = { 0.0f, 0.0f, (float)spaceShip.width / 6, spaceShip.height / 6 };
+	Vector2 position = { 350.0f, 280.0f };
 
 	int playerPosition = screenHeight / 2 - 200 ;
 	int scrollSpeed = 10;
@@ -20,10 +25,12 @@ int main() {
 
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
+		DrawTextureRec(spaceShip, frameRec, position, WHITE);
 		DrawRectangle(screenWidth / 2 - 200, playerPosition, 80, 80, BLUE);
 		EndDrawing();
 	}
 
+	UnloadTexture(spaceShip);
 	CloseWindow();
 
 	return 0;
